9455a9b0fb280cc654079e0f6f9fd5599af20c24
kkhng <52734191+kkhng@users.noreply.github.com>
2020-12-18 23:23:56 +0800

    Add files via upload

---
+++ Simple_Chatbot (Github)/codes/UI/static/js/script.js
@@ -0,0 +1,159 @@
+// on input/text enter--------------------------------------------------------------------------------------
+$('.usrInput').on('keyup keypress', function (e) {
+	var keyCode = e.keyCode || e.which;
+	var text = $(".usrInput").val();
+	if (keyCode === 13) {
+		if (text == "" || $.trim(text) == '') {
+			e.preventDefault();
+			return false;
+		} else {
+			$(".usrInput").blur();
+			setUserResponse(text);
+			send(text);
+			e.preventDefault();
+			return false;
+		}
+	}
+});
+
+
+//------------------------------------- Set user response------------------------------------
+function setUserResponse(val) {
+
+
+	var UserResponse = '<img class="userAvatar" src=' + "./static/img/userAvatar.jpg" + '><p class="userMsg">' + val + ' </p><div class="clearfix"></div>';
+	$(UserResponse).appendTo('.chats').show('slow');
+	$(".usrInput").val('');
+	scrollToBottomOfResults();
+	$('.suggestions').remove();
+}
+
+//---------------------------------- Scroll to the bottom of the chats-------------------------------
+function scrollToBottomOfResults() {
+	var terminalResultsDiv = document.getElementById('chats');
+	terminalResultsDiv.scrollTop = terminalResultsDiv.scrollHeight;
+}
+
+function send(message) {
+	console.log("User Message:", message)
+	$.ajax({
+		url: 'http://localhost:5005/webhooks/rest/webhook',
+		type: 'POST',
+		contentType: 'application/json',
+		data: JSON.stringify({
+			"message": message,
+			"sender": "Me"
+		}),
+		success: function (data, textStatus) {
+			if(data != null){
+					setBotResponse(data);
+			}
+			console.log("Rasa Response: ", data, "\n Status:", textStatus)
+		},
+		error: function (errorMessage) {
+			setBotResponse("");
+			console.log('Error' + errorMessage);
+
+		}
+	});
+}
+
+//------------------------------------ Set bot response -------------------------------------
+function setBotResponse(val) {
+	setTimeout(function () {
+		if (val.length < 1) {
+			//if there is no response from Rasa
+			msg = 'I couldn\'t get that, I am a mood bot. Please type Hi to start.';
+
+			var BotResponse = '<img class="botAvatar" src="./static/img/botAvatar.png"><p class="botMsg">' + msg + '</p><div class="clearfix"></div>';
+			$(BotResponse).appendTo('.chats').hide().fadeIn(1000);
+
+		} else {
+			//if we get response from Rasa
+			for (i = 0; i < val.length; i++) {
+				//check if there is text message
+				if (val[i].hasOwnProperty("text")) {
+					// make clickable link
+					if (val[i].text === "And perhaps a music? click here") {
+						text_music = val[i].text.substr(0,20);
+						click_music = val[i].text.substr(21,30);
+						music = '<a href="https://www.youtube.com/watch?v=c7rCyll5AeY" target="_blank">' + click_music + '</a>';
+
+						var BotResponse = '<img class="botAvatar" src="./static/img/botAvatar.png"><p class="botMsg">' + text_music + ' ' + music + '</p><div class="clearfix"></div>';
+						$(BotResponse).appendTo('.chats').hide().fadeIn(1000);
+					}
+					// make clickable link
+					if (val[i].text === "Sorry that didn't help, find a movie to watch then: click here") {
+						text_movie = val[i].text.substr(0,51)
+						click_movie = val[i].text.substr(52,61)
+						movie = '<a href="https://www.youtube.com/" target="_blank">' + click_movie + '</a>';
+
+						var BotResponse = '<img class="botAvatar" src="./static/img/botAvatar.png"><p class="botMsg">' + text_movie + ' ' + movie + '</p><div class="clearfix"></div>';
+						$(BotResponse).appendTo('.chats').hide().fadeIn(1000);
+					}
+					// return text for others that are not links
+					if (!(val[i].text.includes("click here"))) {
+						var BotResponse = '<img class="botAvatar" src="./static/img/botAvatar.png"><p class="botMsg">' + val[i].text + '</p><div class="clearfix"></div>';
+						$(BotResponse).appendTo('.chats').hide().fadeIn(1000);
+					}
+				}
+
+				//check if there is image
+				if (val[i].hasOwnProperty("image")) {
+					var BotResponse = '<div class="singleCard">' +
+						'<img class="imgcard" src="' + val[i].image + '">' +
+						'</div><div class="clearfix">'
+					$(BotResponse).appendTo('.chats').hide().fadeIn(1000);
+				}
+
+				//check if there is button message
+				if (val[i].hasOwnProperty("buttons")) {
+					addSuggestion(val[i].buttons);
+				}
+
+			}
+			scrollToBottomOfResults();
+		}
+
+	}, 500);
+}
+
+
+// ------------------------------------------ Toggle chatbot -----------------------------------------------
+$('#profile_div').click(function () {
+	$('.profile_div').toggle();
+	$('.widget').toggle();
+	scrollToBottomOfResults();
+});
+
+$('#close').click(function () {
+	$('.profile_div').toggle();
+	$('.widget').toggle();
+});
+
+
+// ------------------------------------------ Suggestions -----------------------------------------------
+
+function addSuggestion(textToAdd) {
+	setTimeout(function () {
+		var suggestions = textToAdd;
+		var suggLength = textToAdd.length;
+		$(' <div class="singleCard"> <div class="suggestions"><div class="menu"></div></div></diV>').appendTo('.chats').hide().fadeIn(1000);
+		// Loop through suggestions
+		for (i = 0; i < suggLength; i++) {
+			$('<div class="menuChips" data-payload=\''+(suggestions[i].payload)+'\'>' + suggestions[i].title + "</div>").appendTo(".menu");
+		}
+		scrollToBottomOfResults();
+	}, 1000);
+}
+
+
+// on click of suggestions, get the value and send to rasa
+$(document).on("click", ".menu .menuChips", function () {
+	var text = this.innerText;
+	var payload= this.getAttribute('data-payload');
+	console.log("button payload: ",this.getAttribute('data-payload'))
+	setUserResponse(text);
+	send(payload);
+	$('.suggestions').remove(); //delete the suggestions 
+});
